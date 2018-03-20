25ba8e3f9a6e0bfeb50635269d0f6a037ab8c311
LaVloZ <lavloz@hotmail.fr>
2018-03-20 15:42:09 +0100

    Modularization of shared module.

---
+++ shared/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module lv.apprentissage.shared {
+	exports lv.merrill.apprentissage.shared.comm;
+	exports lv.merrill.apprentissage.shared.couchdb;
+	exports lv.merrill.apprentissage.shared.evt;
+	exports lv.merrill.apprentissage.shared.repository;
+
+	requires java.json;
+}
